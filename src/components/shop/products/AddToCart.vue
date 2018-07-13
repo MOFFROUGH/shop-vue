771da3ea280f2fde8f316bd86f07f2f0a8979803
MOFFROUGH <moffmu@gmail.com>
2018-07-13 20:36:58 +0300

    added out of stock

---
+++ src/components/shop/products/AddToCart.vue
@@ -0,0 +1,26 @@
+<template>
+  <button type="button" :class="product.inventory?'btn btn-primary':' btn btn-danger'"
+    :disabled="!product.inventory"
+    @click="addToCart(product)">
+    {{text}}
+  </button>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'AddToCart',
+  props: {
+    product: Object
+  },
+  methods: mapActions([
+    'addToCart'
+  ]),
+  computed : {
+    text () {
+      return this.product.inventory?'Add to cart':'Out of Stock'
+    }
+  }
+}
+</script>
